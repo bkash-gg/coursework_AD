a80f49dcee4267cf5d5652dfee7b8d6d3d7d8ad7
sandesh shrestha <sandeshchrestha@gmail.com>
2025-05-05 21:56:11 +0545

    Login and Sigin up button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import BookCatalog from './pages/BookCatalog'
-import './App.css'
-import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BookCatalog from './pages/BookCatalog';
+import Home from './pages/Home';
+import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Home />} />
-        <Route path="/" element={<BookCatalog />} />
-        
+        <Route path="/" element={<Home />} />
+        <Route path="/BookCatalog" element={<BookCatalog />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-md">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
         <h1 className="text-xl font-bold text-red-600">BOOKWORM</h1>
+
         <ul className="hidden md:flex space-x-6 text-gray-700">
-          <li>Home</li>
-          <li>Book Categories</li>
-          <li>Shop</li>
-          <li>Blog</li>
-          <li>Pages</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/book-catalog">Book Catalog</Link></li>
+          <li><Link to="/shop">Shop</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/pages">Pages</Link></li>
         </ul>
-        <input
-          type="text"
-          placeholder="Search"
-          className="hidden md:block px-2 py-1 border rounded"
-        />
+
+        <div className="hidden md:flex items-center space-x-4">
+          <input
+            type="text"
+            placeholder="Search"
+            className="px-2 py-1 border rounded"
+          />
+          <Link
+            to="/login"
+            className="px-3 py-1 border border-red-600 text-red-600 rounded hover:bg-red-100"
+          >
+            Login
+          </Link>
+          <Link
+            to="/signup"
+            className="px-3 py-1 bg-red-600 text-white rounded hover:bg-red-700"
+          >
+            Sign Up
+          </Link>
+        </div>
       </div>
     </nav>
   );
