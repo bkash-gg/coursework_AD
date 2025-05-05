41c9082e05d1ea332f5bb023f141e2a89d2e2e95
sandesh shrestha <sandeshchrestha@gmail.com>
2025-05-05 14:55:03 +0545

    Changing in heading

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>Header</h1>
+      <h1>Company Logo</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
     </header>
   );
 };
