668c56935aa2926995b1e42f0feb42f4b935deca
bkash-gg <np01cp4s230121@islingtoncollege.edu.np>
2025-05-14 03:30:11 +0545

    Final commit for backend API

--- Hubs/AnnouncementHub.cs
+++ Hubs/AnnouncementHub.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace AD_Coursework.Hubs
 {
+    // This hub is used for sending announcements to all connected members.
     [Authorize]
     public class AnnouncementHub : Hub
     {
